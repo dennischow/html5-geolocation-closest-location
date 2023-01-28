d55dcc8cc9b498d953368e601c8e782d08dac3e6
Dennis Chow <dennischow@Denniss-MBP>
2023-01-28 16:56:00 +0800

    Removed manually event trigger and separated dataPopulate to different method

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # html5-geolocation-closest-location
+
+# References:
+https://www.npmjs.com/package/geolib
+https://github.com/manuelbieh/geolib
+https://javascript.info/cookie
+https://www.tabnine.com/academy/javascript/how-to-set-cookies-javascript/
+https://stackoverflow.com/questions/2856513/how-can-i-trigger-an-onchange-event-manually
--- resources/js/main.js
+++ resources/js/main.js
@@ -3,9 +3,9 @@
         {
             id: "c4ca4238a0b923820dcc509a6f75849b",
             name: "[Toronto] VIP Billiards on College",
-            telephone: "4161234567",
-            // googleMapUrl: "https://goo.gl/maps/rva2rSACQR8347pE9",
-            googleMapUrl: "https://www.bbc.com/",
+            phoneNumber: "4161234567",
+            // mapUrl: "https://goo.gl/maps/rva2rSACQR8347pE9",
+            mapUrl: "https://www.bbc.com/",
             address: "842 College St, Toronto, ON M6H 1A2",
             coordinates: {
                 latitude: 43.65444014568791,
@@ -15,10 +15,10 @@
         {
             id: "c81e728d9d4c2f636f067f89cc14862c",
             name: "[Nort York] VIP Billiards and Lounge",
-            telephone: "4169283820",
+            phoneNumber: "4169283820",
             address: "3030 Don Mills Rd E, North York, ON M2J 3C1",
-            // googleMapUrl: "https://goo.gl/maps/5zLEN8NBy6QbxujU9",
-            googleMapUrl: "https://outlook.live.com/owa/",
+            // mapUrl: "https://goo.gl/maps/5zLEN8NBy6QbxujU9",
+            mapUrl: "https://outlook.live.com/owa/",
             coordinates: {
                 latitude: 43.785660243798894,
                 longitude: -79.35176307265208,
@@ -27,10 +27,10 @@
         {
             id: "eccbc87e4b5ce2fe28308fd9f2a7baf3",
             name: "[Toronto] Long & McQuade Musical Instruments",
-            telephone: "4169280394",
+            phoneNumber: "4169280394",
             address: "925 Bloor St W, Toronto, ON M6H 1L5, Canada",
-            // googleMapUrl: "https://goo.gl/maps/QW4bPQYWp4RUpCpY9",
-            googleMapUrl: "https://www.yahoo.com",
+            // mapUrl: "https://goo.gl/maps/QW4bPQYWp4RUpCpY9",
+            mapUrl: "https://www.yahoo.com",
             coordinates: {
                 latitude: 43.661497110634144,
                 longitude: -79.42714916620164,
@@ -126,9 +126,10 @@
         var result = storesData.find((item) => {
             return (item.coordinates.latitude === nearestStore.latitude && item.coordinates.longitude === nearestStore.longitude);
         });
+        console.log("Result object: ", result);
+        console.log("Option text: ", result.name + " - " + result.address);
+
         selectOption(result.id);
-        console.log("result: ", result);
-        console.log("Option: ", result.name + " - " + result.address);
     };
 
     function generateStoreOptions() {
@@ -143,19 +144,19 @@
     function dataPopulate(id) {
         var result = storesData.find((item) => item.id === id);
         eleStoreLocationDisplayText.innerText = result.name + " - " + result.address;
-        eleCallBtn.setAttribute("data-url", "tel:" + result.telephone);
-        eleAddressBtn.setAttribute("data-url", result.googleMapUrl);
-        utils.setCookie("savedLocationId", result.id, {
-            secure: true,
-            "max-age": 3600*24*365.2425,
-        });
-        console.log("Object: ", result);
-        console.log("savedLocationId:", result.id);
+        eleCallBtn.setAttribute("data-url", "tel:" + result.phoneNumber);
+        eleAddressBtn.setAttribute("data-url", result.mapUrl);
+        console.log("dataPopulate: ", result);
     }
 
     function selectionEventHandler() {
         eleStoreLocationSelection.addEventListener("change", function (event) {
             dataPopulate(event.target.value);
+            utils.setCookie("savedLocationId", event.target.value, {
+                secure: true,
+                "max-age": 3600*24*365.2425,
+            });
+            console.log("savedLocationId:", event.target.value);
         }, false);
     }
 
@@ -164,13 +165,14 @@
         eleOptions.forEach(function (option) {
             if (option.value === id) {
                 option.selected = true;
+                dataPopulate(option.value);
             }
         });
         console.log("selectOption: ", id);
 
         // Trigger an event on targeted element
-        var event = new Event("change");
-        eleStoreLocationSelection.dispatchEvent(event);
+        // var event = new Event("change");
+        // eleStoreLocationSelection.dispatchEvent(event);
     }
 
     function actionBtnsEventHandler() {
@@ -187,6 +189,7 @@
         script.setAttribute("src", "https://unpkg.com/geolib@3.3.3/lib/index.js");
         script.setAttribute("async", "");
         script.onload = function () {
+            console.log("New User");
             console.log("script has loaded");
             console.log("geolib: ", geolib);
             getCurrentLocation();
@@ -200,6 +203,7 @@
         actionBtnsEventHandler();
 
         if (utils.getCookie("savedLocationId")) {
+            console.log("Returnd User");
             selectOption(utils.getCookie("savedLocationId"));
             return;
         }
